f11269792bbf7480bf497852394c1db9c3dd8788
Ayan Mondal <ayanmondal1805@gmail.com>
2024-10-07 00:56:52 +0530

    add frontend files

---
+++ frontend/main.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, jsonify, request
+from flask_pymongo import PyMongo
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = os.getenv("MONGO_URL")
+mongo = PyMongo(app)
+
+@app.route("/")
+def home():
+    chats = mongo.db.chats.find({})
+    myChats = [chat for chat in chats]
+    print(myChats)
+    return render_template("index.html", myChats = myChats)
+
+@app.route("/api", methods=["GET", "POST"])
+def qa():
+    if request.method == "POST":
+        print(request.json)
+        question = request.json.get("question")
+        chat = mongo.db.chats.find_one({"question": question})
+        print(chat)
+        if chat:
+            data = {"question": question, "answer": f"{chat['answer']}"}
+            return jsonify(data)
+        else:
+            response = openai.Completion.create(
+                    model="davinci-002",
+                    prompt=question,
+                    temperature=0.7,
+                    max_tokens=256,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0
+                    )
+            print(response)
+            data = {"question": question, "answer": response["choices"][0]["text"]}
+            mongo.db.chats.insert_one({"question": question, "answer": response["choices"][0]["text"]})
+            return jsonify(data)
+    data = {"result": "Thank you! I'm just a machine learning model designed to respond to questions and generate text based on my training data. Is there anything specific you'd like to ask or discuss? "}
+        
+    return jsonify(data)
+
+app.run(debug=True, port=5001)
